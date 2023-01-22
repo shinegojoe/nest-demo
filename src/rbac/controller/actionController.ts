84159e29f2814305c988df203c2833ef576b756c
taka <shinegojoe@gmail.com>
2023-01-21 19:54:41 -0800

    update action swagger

--- src/rbac/controller/actionController.ts
+++ src/rbac/controller/actionController.ts
@@ -2,22 +2,25 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { ActionService } from '../service/actionService';
 import { Action } from '../../entity/rbac/Action';
 import { DeleteResult, InsertResult } from 'typeorm';
-import { ApiBody, ApiParam } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { LogicErrorResponse } from '../../response/logicErrorResp';
 import { errorCode } from '../../response/errorCode';
 
+@ApiTags('action')
 @Controller('actions')
 export class ActionController {
   constructor(private readonly actionService: ActionService) {}
 
   @Get('/list')
+  @ApiOperation({description: "return the action list"})
   async list(): Promise<Array<Action>> {
     const res: Array<Action> = await this.actionService.list();
     return res;
   }
 
+  @ApiOperation({description: "get action by id"})
+  @ApiParam({name: 'id', required: true, description: 'either an integer for the project id or a string for the project name' })
   @Get('/get/:id')
-  @ApiParam({name: 'id', required: true, description: 'either an integer for the project id or a string for the project name', schema: { oneOf: [{type: 'string'}, {type: 'integer'}]}})
   async get(@Param('id') id: number): Promise<Action | LogicErrorResponse> {
     const res = await this.actionService.get(id);
     if(res === null) {
@@ -26,33 +29,33 @@ export class ActionController {
     } else {
       return res;
     }
-
   }
 
+  @ApiOperation({description: "delete action by id"})
+  @ApiParam({name: 'id', required: true})
   @Delete('/delete/:id')
   delete(@Param('id') id: number): Promise<DeleteResult> {
     return this.actionService.delete(id);
   }
 
+
+  @ApiOperation({description: "create action"})
+  @ApiBody({description: "body: Action", type: Action})
   @Post('/create')
-  @ApiBody({description: "body:any someMethod", type: Action})
   create(@Body() body: Action): Promise<InsertResult> {
     return this.actionService.create(body);
   }
 
+
+  @ApiOperation({description: "update action"})
+  @ApiBody({description: "body: Action", type: Action})
   @Put('/update')
   update(@Body() body: Action) {
     return this.actionService.update(body);
   }
 
-  @Get('/errorTest')
-  errorTest() {
-    return {
-      "errorCode": 123,
-      "message": "xxx",
-      "info": 'this is info'
-    }
-  }
-
-
 }
+
+
+
+//   // @ApiParam({name: 'id', required: true, description: 'either an integer for the project id or a string for the project name', schema: { oneOf: [{type: 'string'}, {type: 'integer'}]}})
