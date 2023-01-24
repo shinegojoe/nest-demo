7012e9728708ae2c7428e106d17b228cd074d0d9
taka <shinegojoe@gmail.com>
2023-01-23 19:48:31 -0800

    add error message

--- src/response/errorCode.ts
+++ src/response/errorCode.ts
@@ -1,6 +1,17 @@
 
 
-export const errorCode = {
+const errorCode = {
     NOT_FOUND: 999,
-    NO_AFFECTED: 998
+    NO_AFFECTED: 998,
+    ALREADY_EXISTS: 997,
+    ROLL_BACK: 996
 }
+
+const errorMessage = {
+    999: "not found",
+    998: "no affected",
+    997: "alredy exists",
+    996: "roll back"
+}
+
+export { errorCode, errorMessage }
